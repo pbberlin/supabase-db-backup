1ce944b37a17bccace0326e411f0ea5ae23b149f
pbberlin <pbberlin@users.noreply.github.com>
2024-02-19 01:03:47 +0000

    Supabase backup

--- schema.sql
+++ schema.sql
@@ -16,12 +16,12 @@ ALTER SCHEMA "public" OWNER TO "pg_database_owner";
 
 CREATE TYPE "public"."rec" AS (
 	"i" integer,
-	"t" text
+	"t" "text"
 );
 
 ALTER TYPE "public"."rec" OWNER TO "postgres";
 
-CREATE OR REPLACE FUNCTION "public"."pool_add_account"(arg_num integer) RETURNS bigint
+CREATE OR REPLACE FUNCTION "public"."pool_add_account"("arg_num" integer) RETURNS bigint
     LANGUAGE "plpgsql"
     AS $$
   DECLARE
@@ -34,7 +34,7 @@ CREATE OR REPLACE FUNCTION "public"."pool_add_account"(arg_num integer) RETURNS 
   END;
 $$;
 
-ALTER FUNCTION "public"."pool_add_account"(arg_num integer) OWNER TO "postgres";
+ALTER FUNCTION "public"."pool_add_account"("arg_num" integer) OWNER TO "postgres";
 
 SET default_tablespace = '';
 
@@ -42,8 +42,8 @@ SET default_table_access_method = "heap";
 
 CREATE UNLOGGED TABLE "public"."pool" (
     "id" integer DEFAULT 0 NOT NULL,
-    "provider" text,
-    "description" text,
+    "provider" "text",
+    "description" "text",
     "rpm" integer DEFAULT 0 NOT NULL,
     "rpm_max" integer DEFAULT 0 NOT NULL,
     "rpd" integer DEFAULT 0 NOT NULL,
@@ -51,16 +51,16 @@ CREATE UNLOGGED TABLE "public"."pool" (
     "tpm" integer DEFAULT 0 NOT NULL,
     "tpm_max" integer DEFAULT 0 NOT NULL,
     "tpr_max" integer DEFAULT 0 NOT NULL,
-    "updated_m" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-    "updated_d" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-    "updated" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-    CONSTRAINT "constr_rpm_max" CHECK ((rpm <= rpm_max)),
-    CONSTRAINT "constr_rpm_min" CHECK ((rpm >= 0))
+    "updated_m" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated_d" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    CONSTRAINT "constr_rpm_max" CHECK (("rpm" <= "rpm_max")),
+    CONSTRAINT "constr_rpm_min" CHECK (("rpm" >= 0))
 );
 
 ALTER TABLE "public"."pool" OWNER TO "postgres";
 
-CREATE OR REPLACE FUNCTION "public"."pool_upate"(arg_id integer, decr boolean) RETURNS SETOF public.pool
+CREATE OR REPLACE FUNCTION "public"."pool_upate"("arg_id" integer, "decr" boolean) RETURNS SETOF "public"."pool"
     LANGUAGE "plpgsql"
     AS $$
   BEGIN
@@ -82,9 +82,9 @@ CREATE OR REPLACE FUNCTION "public"."pool_upate"(arg_id integer, decr boolean) R
   END
 $$;
 
-ALTER FUNCTION "public"."pool_upate"(arg_id integer, decr boolean) OWNER TO "postgres";
+ALTER FUNCTION "public"."pool_upate"("arg_id" integer, "decr" boolean) OWNER TO "postgres";
 
-CREATE OR REPLACE FUNCTION "public"."set_of_records"() RETURNS SETOF public.rec
+CREATE OR REPLACE FUNCTION "public"."set_of_records"() RETURNS SETOF "public"."rec"
     LANGUAGE "plv8"
     AS $$
     // plv8.return_next() stores records in an internal tuplestore,
@@ -98,7 +98,7 @@ $$;
 
 ALTER FUNCTION "public"."set_of_records"() OWNER TO "postgres";
 
-CREATE OR REPLACE FUNCTION "public"."sync_users"() RETURNS trigger
+CREATE OR REPLACE FUNCTION "public"."sync_users"() RETURNS "trigger"
     LANGUAGE "plpgsql" SECURITY DEFINER
     SET "search_path" TO 'public'
     AS $$
@@ -120,14 +120,14 @@ $$;
 ALTER FUNCTION "public"."sync_users"() OWNER TO "postgres";
 
 CREATE TABLE IF NOT EXISTS "public"."dialogs" (
-    "id" uuid DEFAULT extensions.uuid_generate_v4() NOT NULL,
+    "id" "uuid" DEFAULT "extensions"."uuid_generate_v4"() NOT NULL,
     "id2" integer NOT NULL,
-    "user_id" uuid NOT NULL,
-    "inserted" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-    "updated" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+    "user_id" "uuid" NOT NULL,
+    "inserted" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
     "name" character varying(255) NOT NULL,
-    "text" text,
-    "messages" jsonb DEFAULT '{}'::jsonb NOT NULL
+    "text" "text",
+    "messages" "jsonb" DEFAULT '{}'::"jsonb" NOT NULL
 );
 
 ALTER TABLE "public"."dialogs" OWNER TO "postgres";
@@ -142,22 +142,22 @@ ALTER TABLE "public"."dialogs" ALTER COLUMN "id2" ADD GENERATED ALWAYS AS IDENTI
 );
 
 CREATE TABLE IF NOT EXISTS "public"."users" (
-    "id" uuid NOT NULL,
+    "id" "uuid" NOT NULL,
     "id2" integer NOT NULL,
-    "inserted" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+    "inserted" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
     "updated" timestamp with time zone,
     "email" character varying(128) NOT NULL,
     "username" character varying(255),
     "source" character varying(255),
     "firstname" character varying(64),
-    "lastname" text,
-    "avatar_url" text,
-    "attrs" jsonb DEFAULT '{}'::jsonb NOT NULL,
-    "assignmentid" text,
-    "hitid" text,
-    "turksubmitto" text,
-    "workerid" text,
-    CONSTRAINT "email_min_length" CHECK ((char_length((email)::text) >= 3))
+    "lastname" "text",
+    "avatar_url" "text",
+    "attrs" "jsonb" DEFAULT '{}'::"jsonb" NOT NULL,
+    "assignmentid" "text",
+    "hitid" "text",
+    "turksubmitto" "text",
+    "workerid" "text",
+    CONSTRAINT "email_min_length" CHECK (("char_length"(("email")::"text") >= 3))
 );
 
 ALTER TABLE "public"."users" OWNER TO "postgres";
@@ -183,29 +183,29 @@ ALTER TABLE ONLY "public"."users"
 ALTER TABLE ONLY "public"."users"
     ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
 
-CREATE INDEX idx_fk_user_id ON public.dialogs USING btree (user_id);
+CREATE INDEX "idx_fk_user_id" ON "public"."dialogs" USING "btree" ("user_id");
 
-CREATE TRIGGER update_timestamp_1 BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION extensions.moddatetime('updated');
+CREATE OR REPLACE TRIGGER "update_timestamp_1" BEFORE UPDATE ON "public"."users" FOR EACH ROW EXECUTE FUNCTION "extensions"."moddatetime"('updated');
 
-CREATE TRIGGER update_timestamp_2 BEFORE UPDATE ON public.dialogs FOR EACH ROW EXECUTE FUNCTION extensions.moddatetime('updated');
+CREATE OR REPLACE TRIGGER "update_timestamp_2" BEFORE UPDATE ON "public"."dialogs" FOR EACH ROW EXECUTE FUNCTION "extensions"."moddatetime"('updated');
 
-CREATE TRIGGER update_timestamp_3 BEFORE UPDATE ON public.pool FOR EACH ROW EXECUTE FUNCTION extensions.moddatetime('updated');
+CREATE OR REPLACE TRIGGER "update_timestamp_3" BEFORE UPDATE ON "public"."pool" FOR EACH ROW EXECUTE FUNCTION "extensions"."moddatetime"('updated');
 
 ALTER TABLE ONLY "public"."dialogs"
-    ADD CONSTRAINT "fk_user" FOREIGN KEY (user_id) REFERENCES public.users(id);
+    ADD CONSTRAINT "fk_user" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id");
 
 ALTER TABLE ONLY "public"."users"
-    ADD CONSTRAINT "users_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE;
+    ADD CONSTRAINT "users_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
 
 ALTER TABLE "public"."dialogs" ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY "public.dialogs-delete" ON "public"."dialogs" FOR DELETE USING ((auth.uid() = user_id));
+CREATE POLICY "public.dialogs-delete" ON "public"."dialogs" FOR DELETE USING (("auth"."uid"() = "user_id"));
 
-CREATE POLICY "public.dialogs-insert" ON "public"."dialogs" FOR INSERT WITH CHECK ((auth.uid() = user_id));
+CREATE POLICY "public.dialogs-insert" ON "public"."dialogs" FOR INSERT WITH CHECK (("auth"."uid"() = "user_id"));
 
-CREATE POLICY "public.dialogs-select" ON "public"."dialogs" FOR SELECT USING ((auth.uid() = user_id));
+CREATE POLICY "public.dialogs-select" ON "public"."dialogs" FOR SELECT USING (("auth"."uid"() = "user_id"));
 
-CREATE POLICY "public.dialogs-update" ON "public"."dialogs" FOR UPDATE USING ((auth.uid() = user_id));
+CREATE POLICY "public.dialogs-update" ON "public"."dialogs" FOR UPDATE USING (("auth"."uid"() = "user_id"));
 
 REVOKE USAGE ON SCHEMA "public" FROM PUBLIC;
 GRANT USAGE ON SCHEMA "public" TO "postgres";
@@ -213,17 +213,17 @@ GRANT USAGE ON SCHEMA "public" TO "anon";
 GRANT USAGE ON SCHEMA "public" TO "authenticated";
 GRANT USAGE ON SCHEMA "public" TO "service_role";
 
-GRANT ALL ON FUNCTION "public"."pool_add_account"(arg_num integer) TO "anon";
-GRANT ALL ON FUNCTION "public"."pool_add_account"(arg_num integer) TO "authenticated";
-GRANT ALL ON FUNCTION "public"."pool_add_account"(arg_num integer) TO "service_role";
+GRANT ALL ON FUNCTION "public"."pool_add_account"("arg_num" integer) TO "anon";
+GRANT ALL ON FUNCTION "public"."pool_add_account"("arg_num" integer) TO "authenticated";
+GRANT ALL ON FUNCTION "public"."pool_add_account"("arg_num" integer) TO "service_role";
 
 GRANT ALL ON TABLE "public"."pool" TO "anon";
 GRANT ALL ON TABLE "public"."pool" TO "authenticated";
 GRANT ALL ON TABLE "public"."pool" TO "service_role";
 
-GRANT ALL ON FUNCTION "public"."pool_upate"(arg_id integer, decr boolean) TO "anon";
-GRANT ALL ON FUNCTION "public"."pool_upate"(arg_id integer, decr boolean) TO "authenticated";
-GRANT ALL ON FUNCTION "public"."pool_upate"(arg_id integer, decr boolean) TO "service_role";
+GRANT ALL ON FUNCTION "public"."pool_upate"("arg_id" integer, "decr" boolean) TO "anon";
+GRANT ALL ON FUNCTION "public"."pool_upate"("arg_id" integer, "decr" boolean) TO "authenticated";
+GRANT ALL ON FUNCTION "public"."pool_upate"("arg_id" integer, "decr" boolean) TO "service_role";
 
 GRANT ALL ON FUNCTION "public"."set_of_records"() TO "anon";
 GRANT ALL ON FUNCTION "public"."set_of_records"() TO "authenticated";
