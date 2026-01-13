260928b620d24ba3702e427d7308dae692b576b8
pbberlin <pbberlin@users.noreply.github.com>
2026-01-13 01:51:46 +0000

    Supabase backup

--- data.sql
+++ data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict PgOZtUHlMizGrb5pe4tFFmEcxqeBLOlWS5EqF5OIrWizZLhtgemH0qepea65G9N
+-- \restrict osgDJagWLW63ePz2fR4LVE1B35fYB1vmTpKYLQWpNLLDXe8GgvoDRf2IjKxr8JE
 
 -- Dumped from database version 15.1 (Ubuntu 15.1-1.pgdg20.04+1)
 -- Dumped by pg_dump version 17.6
@@ -1616,6 +1616,6 @@ SELECT pg_catalog.setval('"supabase_functions"."hooks_id_seq"', 180, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict PgOZtUHlMizGrb5pe4tFFmEcxqeBLOlWS5EqF5OIrWizZLhtgemH0qepea65G9N
+-- \unrestrict osgDJagWLW63ePz2fR4LVE1B35fYB1vmTpKYLQWpNLLDXe8GgvoDRf2IjKxr8JE
 
 RESET ALL;
